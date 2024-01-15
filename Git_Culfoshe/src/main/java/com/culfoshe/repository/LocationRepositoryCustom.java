cb7b294a9365ea698a5ec37f461fcf443c0de8ba
uhyeong11 <sun642758@gmail.com>
2024-01-16 00:57:38 +0900

    Main dto 변경

--- Git_Culfoshe/src/main/java/com/culfoshe/repository/LocationRepositoryCustom.java
+++ Git_Culfoshe/src/main/java/com/culfoshe/repository/LocationRepositoryCustom.java
@@ -1,11 +1,11 @@
 package com.culfoshe.repository;
 
-import com.culfoshe.dto.MainLocationDTO;
+import com.culfoshe.dto.MainViewDTO;
 import org.springframework.data.domain.Page;
 
 import java.awt.print.Pageable;
 
 public interface LocationRepositoryCustom {
 
-    Page<MainLocationDTO> getMainLocationPage(Pageable pageable);
+    Page<MainViewDTO> getMainLocationPage(Pageable pageable);
 }
--- Git_Culfoshe/src/main/java/com/culfoshe/repository/LocationRepositoryCustomImpl.java
+++ Git_Culfoshe/src/main/java/com/culfoshe/repository/LocationRepositoryCustomImpl.java
@@ -1,13 +1,8 @@
 package com.culfoshe.repository;
 
-import com.culfoshe.dto.MainLocationDTO;
-import com.culfoshe.dto.QMainLocationDTO;
-import com.culfoshe.entity.QLocation;
-import com.culfoshe.entity.QLocationImg;
-import com.querydsl.core.QueryResults;
+import com.culfoshe.dto.MainViewDTO;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.awt.print.Pageable;
 
@@ -16,7 +11,7 @@ public class LocationRepositoryCustomImpl implements LocationRepositoryCustom {
     private JPAQueryFactory queryFactory;
 
     @Override
-    public Page<MainLocationDTO> getMainLocationPage(Pageable pageable) {
+    public Page<MainViewDTO> getMainLocationPage(Pageable pageable) {
 
 //        QLocation location = QLocation.location;
 //        QLocationImg locationImg = QLocationImg.locationImg;
