2d909358868cafe900ceb3372e02a8c347b7ad0c
PeterJiseong <wltjd9407@naver.com>
2024-01-18 12:10:19 +0900

    밑작업

---
+++ Git_Culfoshe/src/main/java/com/culfoshe/config/InterceptorChkMember.java
@@ -0,0 +1,39 @@
+package com.culfoshe.config;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.security.Principal;
+
+public class InterceptorChkMember implements HandlerInterceptor {
+    private boolean chkLogin(Principal principal){
+        if(principal != null && principal.getName() !=null){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Principal principal = SecurityContextHolder.getContext().getAuthentication();
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
