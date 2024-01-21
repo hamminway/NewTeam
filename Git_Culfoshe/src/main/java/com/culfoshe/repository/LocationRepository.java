a4d5e8d80c2b7eb9e15627f7230549f54188362b
PeterJiseong <wltjd9407@naver.com>
2024-01-21 16:56:31 +0900

    지성 - csrf해결

--- Git_Culfoshe/src/main/java/com/culfoshe/repository/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.culfoshe.repository;
-
-import com.culfoshe.entity.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long>, LocationRepositoryCustom {
-
-}
