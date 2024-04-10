52cd04fca0c260c475d1ab5e84edcfdce02f3bb8
PeterJiseong <wltjd9407@naver.com>
2024-04-11 03:05:39 +0900

    지성 - 20240411 사이드 메뉴 기능 구현중(디자인 부족.)

--- Git_Culfoshe/src/main/resources/static/script/individual/myPageSide.js
+++ Git_Culfoshe/src/main/resources/static/script/individual/myPageSide.js
@@ -2,10 +2,16 @@
 const cateList = document.querySelector('.cateList');
 const action = document.querySelector('.action');
 const plus = document.querySelector('.cateAddBox');
+const minusSample = document.querySelector('.minusBox');
+const side_bottom = document.querySelector('.side_bottom');
 
 function sideEdit(element){
-    plus.classList.remove('blind');
+    side_bottom.classList.remove('blind');
     element.classList.add('blind');
+    let minusBoxList = document.querySelectorAll('.minusBox');
+    toggleClassListByNodeList(minusBoxList, 'blind', true);
+    minusBoxList[0].classList.add('blind');
+    minusBoxList[1].classList.add('blind');
 }
 function resetFocus(){
     let eachLists = document.querySelectorAll('.eachList');
@@ -14,26 +20,26 @@ function resetFocus(){
         alert("stop!");
         return;
     }
-    for(let i = 0 ; i < eachLists.length ; i++){
-        eachLists[i].classList.remove('focusCate');
-    }
-    for(let i = 0 ; i < eachList_contents.length ; i++){
-        eachList_contents[i].classList.remove('focusCate');
-    }
+    toggleClassListByNodeList(eachLists, 'focusCate', true);
+    toggleClassListByNodeList(eachList_contents, 'focusCate', true);
 }
 
 function addCate(element){
     resetFocus();
     let newP = document.createElement('p');
     let newInput = document.createElement('input');
+    let minusClone = minusSample.cloneNode(true);
     newP.className = 'eachList';
     newP.classList.add('focusCate');
     newInput.className= 'eachList_content';
     newInput.value='제목 없음';
     newInput.classList.add('focusCate');
+    minusClone.classList.remove('blind');
     
+
     let createdP = cateList.appendChild(newP);
     createdP.appendChild(newInput);
+    createdP.appendChild(minusClone);
     createdP.addEventListener('click',(e)=>{
         resetFocus();
         createdP.classList.add('focusCate');
@@ -53,7 +59,20 @@ function deleteCate(element){
     
 }
 
-
+function toggleClassListByNodeList(list, className, boolean){
+    if(list==null){
+        return;
+    }
+    if(boolean){
+        for(let i = 0 ; i< list.length ; i++){
+            list[i].classList.remove(className);
+        }
+    }else{
+        for(let i = 0 ; i< list.length ; i++){
+            list[i].classList.add(className);
+        }
+    }
+}
 
 
 
